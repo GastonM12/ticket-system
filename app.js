261b743da1006c9fd2ec0c38364ca34cb99b201a
GastonM12 <gaston_mori_96@gmail.com>
2026-01-20 15:21:56 -0300

    creacion de base de datos mongodb

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
+import mongoose from "mongoose";
 
 const app = express();
+const DB_URL =
+  process.env.NODE_ENV === "test"
+    ? "mongodb://localhost:27017/ticket-system-db-test"
+    : process.env.DB_URL || "mongodb://localhost:27017/ticket-system";
 
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).send("hellow word gaston si")
+mongoose.connect(DB_URL).then(()=>{
+    console.log("connected to mongoDB: "+DB_URL);
+}).catch((err)=>{
+    console.error(err);
 })
+
+app.get("/", (req, res) => {
+  res.status(200).send("hellow word gaston si");
+});
 
 export default app;
