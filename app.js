ba4ce87f40159293936391d6a04e32fe9c399771
GastonM12 <gaston_mori_96@gmail.com>
2026-01-21 16:57:12 -0300

    autenticacion

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
 import mongoose from "mongoose";
+import usersRoutes from "./routes/usersRoutes.js";
+import ticketsRoutes from "./routes/ticketsRoutes.js";
 
 const app = express();
 const DB_URL =
@@ -12,14 +14,20 @@ const DB_URL =
 app.use(morgan("dev"));
 app.use(express.json());
 
-mongoose.connect(DB_URL).then(()=>{
-    console.log("connected to mongoDB: "+DB_URL);
-}).catch((err)=>{
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log("connected to mongoDB: " + DB_URL);
+  })
+  .catch((err) => {
     console.error(err);
-})
+  });
 
-app.get("/", (req, res) => {
-  res.status(200).send("hellow word gaston si");
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
 });
+
+app.use("/api/users", usersRoutes);
+app.use("/api/tickets", ticketsRoutes)
 
 export default app;
---
+++ routes/ticketsRoutes.js
@@ -0,0 +1,73 @@
+import express from "express";
+import Ticket from "../model/ticket.js";
+import  auth from "../middlewares/auth.js";
+import admin from "../middlewares/admind.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const tickets = await Ticket.find();
+    res.status(200).json(tickets);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+router.get("/:id", async (req, res) => {
+  try {
+    const ticket = await Ticket.findById(req.params.id);
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+    res.status(200).json(ticket);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post("/", auth ,async (req, res) => {
+  
+  const ticket = new Ticket({
+    user: req.user._id,
+    title: req.body.title,
+    description: req.body.description,
+    priority: req.body.priority,
+    status: req.body.status,
+  });
+
+  try {
+    const newTicket = await ticket.save();
+
+    res.status(201).json({ newTicket });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.put("/:id",auth, async (req, res) => {
+  try {
+    const ticket = await Ticket.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+    return res.status(200).json(ticket);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+
+router.delete("/:id", [auth,admin],async (req, res) => {
+  try {
+    const ticket = await Ticket.findByIdAndDelete(req.params.id);
+    if(!ticket){
+        return res.status(404).json({ message: "Ticket not found" });
+    }
+    return res.status(200).json({ message: "Ticket deleted successfully" });
+  } catch (err){
+    return res.status(500).json({ message: err.message });
+  }
+});
+
+export default router;
